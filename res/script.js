8b1516ddb9452d747dc9644b52b27e3b3649d5e1
thakkaha <harsh.thakkar@sheridancollege.ca>
2026-03-30 13:30:18 -0400

    v2

--- res/script.js
+++ res/script.js
@@ -1,16 +1,18 @@
-$(document).on('pagebeforeshow', '#two', function(){      
+$(document).on('pagebeforeshow', '#two', function () {
     $('#mydata').empty();
-    
-            $('#mydata').append('<img src="res/'+myInfo.id+'.jpg" width="100%" height="100%">');  
-document.getElementById(myInfo.id).play();
-       
+
+    $('#mydata').append('<img src="res/' + myInfo.id + '.jpg" width="100%" height="100%">');
+    musicelements = document.getElementsByTagName("audio");
+    for (i = 0; i < musicelements.length; i++) musicelements[i].pause();
+    document.getElementById(myInfo.id).play();
+
 });
 
-$(document).on('vclick', '#mylist li a', function(){  
+$(document).on('vclick', '#mylist li a', function () {
     myInfo.id = $(this).attr('data-id');
-    $.mobile.changePage( "#two", { transition: "slide", changeHash: false });
+    $.mobile.changePage("#two", { transition: "slide", changeHash: false });
 });
 
 var myInfo = {
-    id : null
+    id: null
 }
